2ec7caf373bbc7b2fc1bff6dad3ba4c35483e75c
AnPham <anphambk@gmail.com>
2016-06-16 12:59:42 +0800

    new

---
+++ code/load_dir.m
@@ -0,0 +1,14 @@
+fnames = dir('notations');
+numfids = length(fnames);
+
+X_train = [];
+t_train = [];
+for k = 1:numfids
+    symbol = fnames(k).name;
+    if(strcmp(symbol, '.') || strcmp(symbol, '..'))
+        continue;
+    end
+    [X, t] = read_images(40, 40, symbol, strcat('notations/', symbol));
+    X_train = [X_train; X];
+    t_train = [t_train; t];
+end
