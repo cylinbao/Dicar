fb48a0f27ba7dbcd1595a2c07ef164f0134e65e1
AnPham <anphambk@gmail.com>
2016-06-16 13:42:19 +0800

    new

---
+++ code/script.m
@@ -0,0 +1,21 @@
+clear; close all; clc;
+
+im = imread('im.png');
+if(ndims(im) == 3)
+    im = rgb2gray(im);
+end
+
+thresh = graythresh(im);
+bw = im2bw(im, thresh);
+bw = bwareaopen(bw, 100);
+bw_ = ~bw;
+
+CC = bwconncomp(bw_);
+S = regionprops(CC, 'Image');
+
+numfids = length(S);
+for k = 1:numfids
+    sub_im = S(k).Image;
+    imshow(sub_im);
+    pause(1);
+end
