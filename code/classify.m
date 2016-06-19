3baf5ae2bebf95ab34457f0b03c2dd748a2e8a67
Chien Yu Lin <myislin@gmail.com>
2016-06-19 23:13:38 +0800

    able to handle mulitple rows input for classify.m

--- code/classify.m
+++ code/classify.m
@@ -4,6 +4,6 @@ function [predict acc est] = classify(x)
 addpath './libsvm/libsvm_unix'
 load './classifier/nuSVModel.mat';
 
-[predict acc est] = svmpredict(0, x, svm_model, '-b 1');
+[predict acc est] = svmpredict(zeros(size(x,1),1), x, svm_model, '-b 1');
 
 end
