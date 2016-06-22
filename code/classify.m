3b06e6aa40bd82c8aedcf109c512b2225fbf7feb
AnPham <anphambk@gmail.com>
2016-06-22 15:28:29 +0800

    remove HOG

--- code/classify.m
+++ code/classify.m
@@ -24,14 +24,14 @@ predict_vec = [predict_r, predict_l, predict_p];
 predict_rt = predict_r - (predict_r > 16);
 probability_r = est_r(predict_rt);
 
-predict_ht = predict_h - (predict_h > 16);
-probability_h = est_h(predict_ht);
-
 predict_lt = predict_l - (predict_l > 16);
 probability_l = est_l(predict_lt);
+
+predict_pt = predict_p - (predict_p > 16);
+probability_p = est_p(predict_pt);
 % 
-% predict_pt = predict_p - (predict_p > 16);
-% probability_p = est_p(predict_pt);
+% predict_ht = predict_h - (predict_h > 16);
+% probability_h = est_h(predict_ht);
 
 % probability_vec = [probability_r, probability_l, probability_p, probability_h];
 probability_vec = [probability_r, probability_l, probability_p];
